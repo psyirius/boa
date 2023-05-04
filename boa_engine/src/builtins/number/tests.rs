60d518c63b7bae619bf63f73791eeb45f68efc22
Haled Odat <halidodat@gmail.com>
2023-05-04 22:24:23 +0200

    Add test for issue #2609

--- boa_engine/src/builtins/number/tests.rs
+++ boa_engine/src/builtins/number/tests.rs
@@ -479,3 +479,12 @@ fn issue_2717() {
         TestAction::assert_eq("(0.23046743672210102).toString(36)", "0.8aoosla2phj"),
     ]);
 }
+
+// https://github.com/boa-dev/boa/issues/2609
+#[test]
+fn issue_2609() {
+    run_test_actions([
+        TestAction::assert_eq("(1.25).toFixed(1)", "1.3"),
+        TestAction::assert_eq("(1.35).toFixed(1)", "1.4"),
+    ]);
+}
